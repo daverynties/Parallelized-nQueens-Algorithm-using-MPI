582293a6fd28ccd10e80a0c26fa811e0c0da007f
David Rynties <ryntiesd@eos13.cis.gvsu.edu>
2016-12-04 14:08:15 -0500

    Added required MPI functions

--- nQueenMPI.c
+++ nQueenMPI.c
@@ -2,6 +2,8 @@
 //#include <iostream>
 #include <stdio.h> 
 #include <time.h>
+#include <mpi.h>
+#include <unistd.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -12,45 +14,55 @@ int totalSolutions = 0;
 int noConflicts(int q[], int n);
 void staticQueensMPI(int q[], int n);
 
-int main(void) {
-   clock_t start, finish, time_in_seconds;
+int main(int argc, char * argv[]) {
+    
+    clock_t start, finish, time_in_seconds;
+    int my_rank, source, num_nodes;
+    int board[N];
+    int i;
 
-   int i;
-   int board[N];
 
-   start = clock();
-   staticQueensMPI(board, 1);
-   finish = clock();
-   unsigned long millis = (finish - start) * 1000 / CLOCKS_PER_SEC;
-   printf("\nTotal Number of Solutions: %d\nRun Time: %lu\n\n", totalSolutions, millis);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_nodes);
 
-   //system("pause");
-   return 0;
+    start = clock();
+    staticQueensMPI(board, 1);
+    finish = clock();
+    unsigned long millis = (finish - start) * 1000 / CLOCKS_PER_SEC;
+    printf("count: %d\ntime: %lu\n\n", totalSolutions, millis);
+
+    //system("pause");
+    MPI_Finalize();
+    return 0;
 }
 
 void staticQueensMPI(int board[], int currentRow) {
 
-   int i;
-   if (currentRow == N) {
-      totalSolutions++;
-   } else {
-      board[0] = 8; //set static for parallelization
-      for (i = 0; i < N; i++) {
-         board[currentRow] = i;
-         if (noConflicts(board, currentRow))
-            staticQueensMPI(board, currentRow + 1);
-      }
-   }
+    int i;
+    if (currentRow == N) {
+        totalSolutions++;
+    } else {
+        board[0] = 8; //set static for parallelization
+        for (i = 0; i < N; i++) {
+            board[currentRow] = i;
+            if (noConflicts(board, currentRow))
+                staticQueensMPI(board, currentRow + 1);
+        }
+    }
 }
 
 int noConflicts(int board[], int currentRow) {
 
-   int i;
-   for (i = 0; i < currentRow; i++) {
-      if (board[i] == board[currentRow])
-         return FALSE; // same column
-      if (abs(board[i] - board[currentRow]) == (currentRow - i))
-         return FALSE; // same diagonal
-   }
-   return TRUE;
+    int i;
+    
+    for (i = 0; i < currentRow; i++) {
+        if (board[i] == board[currentRow])
+            return FALSE; // same column
+        if (abs(board[i] - board[currentRow]) == (currentRow - i))
+            return FALSE; // same diagonal
+    }
+    return TRUE;
 }
+
+
---
+++ nQueenMPI_back.c
@@ -0,0 +1,56 @@
+//#include "stdafx.h"
+//#include <iostream>
+#include <stdio.h> 
+#include <time.h>
+
+#define TRUE 1
+#define FALSE 0
+#define N 13
+
+int totalSolutions = 0;
+
+int noConflicts(int q[], int n);
+void staticQueensMPI(int q[], int n);
+
+int main(void) {
+   clock_t start, finish, time_in_seconds;
+
+   int i;
+   int board[N];
+
+   start = clock();
+   staticQueensMPI(board, 1);
+   finish = clock();
+   unsigned long millis = (finish - start) * 1000 / CLOCKS_PER_SEC;
+   printf("\nTotal Number of Solutions: %d\nRun Time: %lu\n\n", totalSolutions, millis);
+
+   //system("pause");
+   return 0;
+}
+
+void staticQueensMPI(int board[], int currentRow) {
+
+   int i;
+   if (currentRow == N) {
+      totalSolutions++;
+   } else {
+      board[0] = 8; //set static for parallelization
+      for (i = 0; i < N; i++) {
+         board[currentRow] = i;
+         if (noConflicts(board, currentRow))
+            staticQueensMPI(board, currentRow + 1);
+      }
+   }
+}
+
+int noConflicts(int board[], int currentRow) {
+
+   int i;
+   for (i = 0; i < currentRow; i++) {
+      if (board[i] == board[currentRow])
+         return FALSE; // same column
+      if (abs(board[i] - board[currentRow]) == (currentRow - i))
+         return FALSE; // same diagonal
+   }
+   return TRUE;
+}
